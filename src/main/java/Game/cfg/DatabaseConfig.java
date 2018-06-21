6f54201c70bed6645b4d9d81ae187864b884da51
assistant16 <vladislav.bogdanovich@mail.ru>
2018-06-21 16:55:02 +0300

    Commit v.7

---
+++ src/main/java/Game/cfg/DatabaseConfig.java
@@ -0,0 +1,20 @@
+package Game.cfg;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by alsa on 20.11.2016.
+ */
+//@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
